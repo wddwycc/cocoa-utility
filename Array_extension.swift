2e772934ac4074b12e5def0fa4285285d1e95588
闻端Carl <wddwyss@gmail.com>
2016-02-01 15:40:27 +0800

    Create Array_extension.swift

---
+++ Array_extension.swift
@@ -0,0 +1,13 @@
+extension Array {
+    mutating func removeObject<U: Equatable>(object: U) -> Bool {
+        for (idx, objectToCompare) in self.enumerate() {  //in old swift use enumerate(self)
+            if let to = objectToCompare as? U {
+                if object == to {
+                    self.removeAtIndex(idx)
+                    return true
+                }
+            }
+        }
+        return false
+    }
+}
