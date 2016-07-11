f358b24bb83b64a3e3ee09f6a2335ba1aa9811d6
闻端Carl <wddwyss@gmail.com>
2016-07-11 23:05:23 +0800

    update

--- NSDate_extension.swift
+++ NSDate_extension.swift
@@ -2,23 +2,23 @@ import Foundation
 import UIKit
 
 extension NSDate{
-    func dateComponents()->NSDateComponents{
+    func dateComponents() -> NSDateComponents{
         let calendar = NSCalendar.currentCalendar()
         let dateComponents = calendar.components([NSCalendarUnit.Year, NSCalendarUnit.Month, NSCalendarUnit.Day, NSCalendarUnit.Hour, NSCalendarUnit.Minute, NSCalendarUnit.Second], fromDate: self)
         return dateComponents
     }
-    func intervelToNow()->String{
-        if(self.compare(NSDate()) == NSComparisonResult.OrderedDescending){
+    func timeIntervelToNow() -> String{
+        if self.compare(NSDate()) == NSComparisonResult.OrderedDescending {
             return "刚才"
         }
         let interval = NSDate().timeIntervalSinceDate(self)
-        if(interval < 60 * 60){
+        if interval < 60 * 60 {
             return "\(Int(interval / 60))分钟前"
         }
-        if(interval < 60 * 60 * 24){
+        if interval < 60 * 60 * 24 {
             return "\(Int(interval / 3600))小时前"
         }
-        if(interval < 60 * 60 * 24 * 365){
+        if interval < 60 * 60 * 24 * 365 {
             return "\(Int(interval) / Int(60 * 60 * 24)))天前"
         }
         return "\(Int(interval) / Int(60 * 60 * 24 * 365)))年前"
