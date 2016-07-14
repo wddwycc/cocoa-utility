33b436acbecbbb89d5bea8c71a43aaa6c5554043
闻端Carl <wddwyss@gmail.com>
2016-07-14 22:14:26 +0800

    update

---
+++ UIView_extension.swift
@@ -0,0 +1,15 @@
+//
+//  UIView_extension.swift
+//  zhaoyao
+//
+//  Created by 闻端 on 16/7/14.
+//  Copyright © 2016年 uki. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func autolayoutView() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
