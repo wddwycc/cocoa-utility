ba147ea94280aaf6a3cc0bcf1ce8c4a231a010ff
闻端Carl <wddwyss@gmail.com>
2016-07-28 14:45:45 +0800

    update

--- UIView_extension.swift
+++ UIView_extension.swift
@@ -13,3 +13,24 @@ extension UIView {
         self.translatesAutoresizingMaskIntoConstraints = false
     }
 }
+
+extension UIView {
+    func shake() {
+        let anim = CAKeyframeAnimation( keyPath:"transform" )
+        anim.values = [
+            NSValue( CATransform3D: CATransform3DMakeTranslation(-5, 0, 0 ) ),
+            NSValue( CATransform3D: CATransform3DMakeTranslation( 5, 0, 0 ) )
+        ]
+        anim.autoreverses = true
+        anim.repeatCount = 2
+        anim.duration = 7/100
+        self.layer.addAnimation(anim, forKey: nil)
+    }
+    func jump() {
+        transform = CGAffineTransformMakeTranslation(0, -10)
+        UIView.animateWithDuration(0.4, delay: 0, usingSpringWithDamping: 0.25, initialSpringVelocity: 1,
+                                   options: .CurveEaseInOut, animations: {
+                                    self.transform = CGAffineTransformIdentity
+        }, completion: nil)
+    }
+}
